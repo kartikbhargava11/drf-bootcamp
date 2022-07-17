61a784093889eb0246d3821acb2134bb48769e0c
kartikbhargava11 <kartikbhargava1997@gmail.com>
2022-07-17 19:18:25 +0530

    Django model instance as API Response & Dictionary

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,17 @@
 import json
 
+from django.forms.models import model_to_dict
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    # request is instance of Django HttpRequest - Check Docs
-    # dir(request)   
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # takes string of JSON data and coverts it into a Python dictionary
-    except:
-        pass
-    print(data)
-    data["params"] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        # data["title"] = model_data.title
+        # data["content"] = model_data.content
+        # data["price"] = model_data.price
+        data = model_to_dict(model_data, fields=["id", "title"])
     return JsonResponse(data)
